9122b42d286b3e8368d87a4da800d5492ec9395b
mehdi-fakihani <mehdi.fakihani@outlook.com>
2016-03-07 16:30:08 +0100

    Added Player
    The player starts at the bottom and can move toward three directions:
    -Left
    -Up
    -Right

--- Greenfoot TP/MyWorld.java
+++ Greenfoot TP/MyWorld.java
@@ -2,22 +2,29 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class MyWorld here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class MyWorld extends World
 {
-    private static final int CELL_SIZE = 10;
-    private static final int WORLD_WIDTH = 80; //in cell
-    private static final int WORLD_HEIGHT = 60; //in cell
+    private static final int CELL_SIZE = 20;
+    private static final int WORLD_WIDTH = 40; //in cell
+    private static final int WORLD_HEIGHT = 30; //in cell
+    
+    
     /**
      * Constructor for objects of class MyWorld.
-     * 
+     *
      */
     public MyWorld()
-    {    
+    {
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(WORLD_WIDTH, WORLD_HEIGHT, CELL_SIZE);
+        Greenfoot.setSpeed(60);
+        
+        Player player = new Player();
+        addObject(player, WORLD_WIDTH/2, WORLD_HEIGHT-1); 
+        
     }
 }
---
+++ Greenfoot TP/Player.java
@@ -0,0 +1,53 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Player here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Player extends Actor
+{
+    private int delay = 0;
+    
+    public Player()
+    {
+        super();
+        setRotation(270);
+    }
+    
+    
+    public void act() 
+    {
+        checkInput();
+    }
+    
+    public void checkInput()
+    {
+        if(delay == 0)
+        {
+            if(Greenfoot.isKeyDown("right"))
+            {
+                setRotation(0);
+                move(1);
+            }
+            
+            if(Greenfoot.isKeyDown("left"))
+            {
+                setRotation(180);
+                move(1);
+            }
+            
+            if(Greenfoot.isKeyDown("up"))
+            {
+                setRotation(270);
+                move(1);
+            }
+            
+            delay = 20;
+        }
+        
+        else
+            delay--;
+    }
+}
