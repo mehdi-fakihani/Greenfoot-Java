1b4ea667b9ab65bc0c0682611acbe43e362a3e12
mehdi-fakihani <mehdi.fakihani@outlook.com>
2016-03-07 15:38:27 +0100

    Add greenfoot project directory

---
+++ Greenfoot TP/MyWorld.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+    private static final int CELL_SIZE = 10;
+    private static final int WORLD_WIDTH = 80; //in cell
+    private static final int WORLD_HEIGHT = 60; //in cell
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(WORLD_WIDTH, WORLD_HEIGHT, CELL_SIZE);
+    }
+}
